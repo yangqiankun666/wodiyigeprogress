5ef22b184c1957bfa467c3518b9e5c49321b064e
yangqiankun666 <1960466252@qq.com>
2018-05-13 09:02:12 +0800

    我的第一个git

--- bookmvnv1/src/main/java/cn/edu/nyist/bookmvnv1/biz/LoginBiz.java
+++ bookmvnv1/src/main/java/cn/edu/nyist/bookmvnv1/biz/LoginBiz.java
@@ -3,5 +3,7 @@ package cn.edu.nyist.bookmvnv1.biz;
 public interface LoginBiz {
 
 	boolean findLoginByNameAndPwd(String name, String pwd);
-
+public static void main(String[] args) {
+	System.out.println("hello whorld");
+}
 }
